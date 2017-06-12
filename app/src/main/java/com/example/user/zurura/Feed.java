c9be7fbd61e24a12a7f3d7c05d7fd5b7f2e07ddb
PaulChege <pchegenjenga@gmail.com>
2017-06-12 20:31:06 +0300

    homeactivity

---
+++ app/src/main/java/com/example/user/zurura/Feed.java
@@ -0,0 +1,20 @@
+package com.example.user.zurura;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by paul on 12/06/2017.
+ */
+
+public class Tab3Feed extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3_feed, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/user/zurura/Profile.java
@@ -0,0 +1,21 @@
+package com.example.user.zurura;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by paul on 12/06/2017.
+ */
+
+
+public class Tab1Profile extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1_profile, container, false);
+        return rootView;
+    }
+}
