a049730f6cf75ad4fe861e5fdffaca7386bc473d
PaulChege <pchegenjenga@gmail.com>
2017-06-14 08:47:44 +0300

    map changes

--- app/src/main/java/com/example/user/zurura/Status.java
+++ app/src/main/java/com/example/user/zurura/Status.java
@@ -1,14 +1,18 @@
 package com.example.user.zurura;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +24,11 @@ import android.widget.ImageView;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -31,7 +40,7 @@ import static android.app.Activity.RESULT_OK;
  * Created by paul on 12/06/2017.
  */
 
-public class Status extends Fragment implements OnConnectionFailedListener{
+public class Status extends Fragment implements OnConnectionFailedListener {
     ImageButton imageButton;
     ImageView imageView;
     DatabaseReference firebaseDatabase;
@@ -47,23 +56,60 @@ public class Status extends Fragment implements OnConnectionFailedListener{
     String userEmail;
     private GoogleApiClient mGoogleApiClient;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab2_status, container, false);
-        imageButton = (ImageButton)rootView.findViewById(R.id.imageButton);
-        imageView = (ImageView)rootView.findViewById(R.id.imageView);
+        imageButton = (ImageButton) rootView.findViewById(R.id.imageButton);
+        imageView = (ImageView) rootView.findViewById(R.id.imageView);
         descText = (EditText) rootView.findViewById((R.id.descText));
-        postButton=(Button)rootView.findViewById(R.id.post);
+        postButton = (Button) rootView.findViewById(R.id.post);
         firebaseDatabase = FirebaseDatabase.getInstance().getReference();
         user = FirebaseAuth.getInstance().getCurrentUser();
         mGoogleApiClient = new GoogleApiClient
-                .Builder(this)
+                .Builder(getActivity())
                 .addApi(Places.GEO_DATA_API)
                 .addApi(Places.PLACE_DETECTION_API)
-                .enableAutoManage(this, this)
+                .enableAutoManage(getActivity(), this)
                 .build();
 
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+//
+//                ActivityCompat.requestPermissions(getActivity(),
+//                        new String[]{Manifest.permission.READ_CONTACTS},
+//                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+                .getCurrentPlace(mGoogleApiClient, null);
+        result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+            @Override
+            public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                    Log.i("new", String.format("Place '%s' has likelihood: %g",
+                            placeLikelihood.getPlace().getName(),
+                            placeLikelihood.getLikelihood()));
+                }
+                likelyPlaces.release();
+            }
+        });
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
